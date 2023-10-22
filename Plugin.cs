84f6147e1d36ce78671be26a388ed3207cb67b55
EzeTxle <60270488+EzeTxle@users.noreply.github.com>
2023-10-22 01:40:59 -0300

    ad

--- Plugin.cs
+++ Plugin.cs
@@ -18,7 +18,7 @@ namespace Main
     [Mod.SalemMod]
     public class Main
     {
-        static public Role[] rolesWithDeathAnim = 
+        static public Role[] rolesWithDeathAnim =
         {
             Role.ARSONIST,
             Role.SERIALKILLER,
@@ -27,47 +27,54 @@ namespace Main
         };
         public static void Start()
         {
-            Debug.Log("Working?"); 
+            Debug.Log("Working?");
         }
-        public static Role GetRandomRole(){
-        System.Random r = new();
-        return rolesWithDeathAnim[r.Next(0, rolesWithDeathAnim.Length)];
-    }
+        public static Role GetRandomRole()
+        {
+            System.Random r = new();
+            return rolesWithDeathAnim[r.Next(0, rolesWithDeathAnim.Length)];
+        }
     }
     [HarmonyPatch(typeof(GameSimulation), "HandlePlayCinematic")]
-    class RandomizeInGameDeathAnimations{
-        static public void Prefix(ref PlayCinematicMessage message){
-            
-            if(!ModSettings.GetBool("Random Death Animations", "JAN.improbabledeadanims")) return;
+    class RandomizeInGameDeathAnimations
+    {
+        static public void Prefix(ref PlayCinematicMessage message)
+        {
+
+            if (!ModSettings.GetBool("Random Death Animations", "JAN.improbabledeadanims")) return;
             var data = message.cinematicEntry.GetData();
-            if(data.cinematicType == CinematicType.Attacked){
+            if (data.cinematicType == CinematicType.Attacked)
+            {
                 (data as AttackedCinematicData).attackerRole = Main.GetRandomRole();
             }
         }
     }
     [HarmonyPatch(typeof(GameSimulation), "HandleOnGameInfoChanged")]
-    class ImprobableDeadAnims{
+    class ImprobableDeadAnims
+    {
         public static bool smart = false;
         public static float sprobability = 0;
         public static float probability = 0;
         [HarmonyPostfix]
-        public static void AddMessage(GameSimulation __instance, GameInfo gameInfo){
-            if(!ModSettings.GetBool("Improbable Death Animations","JAN.improbabledeadanims")) return;
-            if(gameInfo.gamePhase != GamePhase.PLAY)return;
-            if(!Pepper.AmIAlive()) return;
+        public static void AddMessage(GameSimulation __instance, GameInfo gameInfo)
+        {
+            if (!ModSettings.GetBool("Improbable Death Animations", "JAN.improbabledeadanims")) return;
+            if (gameInfo.gamePhase != GamePhase.PLAY) return;
+            if (!Pepper.AmIAlive()) return;
             PlayPhase playPhase = __instance.playPhaseState.Get().playPhase;
-            if(playPhase == PlayPhase.FIRST_DISCUSSION) {
-                probability = float.Parse(ModSettings.GetString("Probability of the Death Anims","JAN.improbabledeadanims"), CultureInfo.InvariantCulture.NumberFormat) * 1000;
-                sprobability = float.Parse(ModSettings.GetString("Probability added per night","JAN.improbabledeadanims"), CultureInfo.InvariantCulture.NumberFormat) * 1000;
+            if (playPhase == PlayPhase.FIRST_DISCUSSION)
+            {
+                probability = float.Parse(ModSettings.GetString("Probability of the Death Anims", "JAN.improbabledeadanims"), CultureInfo.InvariantCulture.NumberFormat) * 1000;
+                sprobability = float.Parse(ModSettings.GetString("Probability added per night", "JAN.improbabledeadanims"), CultureInfo.InvariantCulture.NumberFormat) * 1000;
                 smart = ModSettings.GetBool("Use smart probabilities", "JAN.improbabledeadanims");
                 return;
             }
-            if(playPhase != PlayPhase.NIGHT) return;
-            if(smart) probability = Mathf.Clamp(probability + sprobability, 0, 100000);
+            if (playPhase != PlayPhase.NIGHT) return;
+            if (smart) probability = Mathf.Clamp(probability + sprobability, 0, 100000);
             System.Random r = new();
             Service.Home.UserService.Settings.SetShowCinematics(r.Next(0, 100000) < probability);
         }
     }
-        
+
 
 }
